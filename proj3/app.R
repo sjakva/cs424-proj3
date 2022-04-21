affc0ce0d9969d94634dad8fa8de13e96ce9fd26
Shoaib Jakvani <sjakva3@uic.edu>
2022-04-21 01:14:07 -0500

    fourth bar chart

--- proj3/app.R
+++ proj3/app.R
@@ -207,7 +207,7 @@ ui <- dashboardPage(
           tabPanel("Day of year",    plotOutput("daysOfYearPlot", width = "100%")),
           tabPanel("Hour of day",    plotOutput("hoursByDayPlot", width = "100%")),
           tabPanel("Day of week",    plotOutput("weekDayPlot", width = "100%")),
-          tabPanel("Month", "the distribution of the number of rides by month of year (Jan through Dec)"),
+          tabPanel("Month",          plotOutput("monthOfYearPlot", width = "100%")),
           tabPanel("Mileage", "the distribution of the number of rides by binned mileage (with an appropriate number of bins)"),
           tabPanel("Trip time", "the distribution of the number of rides by binned trip time (with an appropriate number of bins)")
         )
@@ -220,7 +220,7 @@ ui <- dashboardPage(
           tabPanel("Day of year",    dataTableOutput("daysOfYearTable")),
           tabPanel("Hour of day",    dataTableOutput("hoursByDayTable")),
           tabPanel("Day of week",    dataTableOutput("weekDayTable")),
-          tabPanel("Month", "the distribution of the number of rides by month of year (Jan through Dec)"),
+          tabPanel("Month",          dataTableOutput("monthOfYearTable")),
           tabPanel("Mileage", "the distribution of the number of rides by binned mileage (with an appropriate number of bins)"),
           tabPanel("Trip time", "the distribution of the number of rides by binned trip time (with an appropriate number of bins)")
         ),
@@ -379,6 +379,30 @@ server <- function(input, output, session) {
     dataWeekDay2
   }))
   # ---------------------------------------------------------------------- //
+  
+  
+  
+  # distribution of the number of rides by month of year (Jan through Dec)
+  # ---------------------------------------------------------------------- //
+  #   //  Chart   //
+  output$monthOfYearPlot <- renderPlot({
+    
+    ggplot(dataHoursByMonth, aes(x = Month, y = Count)) + geom_bar(stat = "identity", fill = "#ffad33", width = 0.8) +
+      labs(x = "Month", y = "Total number of rides") + theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5, size=20), axis.title=element_text(size=12), axis.text.x = element_text(angle = 0, vjust = 0.5, hjust=1)) + 
+      coord_cartesian(expand = FALSE) #+ scale_x_discrete(labels=c(0:23))
+    # scale_x_date(date_breaks = "day", date_labels = "%b. %d") + coord_cartesian(expand = FALSE)
+  })
+  # ---------------------------------------------------------------------- //
+  #   //  Table   //
+  output$monthOfYearTable <- DT::renderDataTable(DT::datatable({
+    
+    # dataHoursByDay$Hour <- format(dataDaysByYear$Date, "%b. %d")
+    dataHoursByMonth$Count <- formatC(dataHoursByMonth$Count, big.mark = ",")
+    dataHoursByMonth
+  }))
+  # ---------------------------------------------------------------------- //
+  
   
   
 }
